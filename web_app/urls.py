d627502fe07d69ffb9f7060e023511082752def2
AnthonySinitsa <itonyemail@gmail.com>
2023-11-09 02:55:19 +0000

    templates aded and login and logout urls added

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import SignUpView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('signup/', SignUpView.as_view(), name='signup'),
+  path('login/', auth_views.LoginView.as_view(), name='login'),
+  path('logout/', auth_views.LoginView.as_view(), name='logout'),
 ]
