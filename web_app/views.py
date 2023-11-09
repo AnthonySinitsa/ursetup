6cdeb953a93a79f0279a48e7369e7fe0811a2b62
AnthonySinitsa <itonyemail@gmail.com>
2023-11-09 02:46:35 +0000

    views.py

--- web_app/views.py
+++ web_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class SignUpView(generic.CreateView):
+  form_class = CustomUserCreationForm
+  success_url = reverse_lazy('login') # Redirect to login page after signup
+  template_name = 'registration/signup.html'
