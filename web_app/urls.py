73537cb9dfdc2d22003c7841c2aa7d27b431ef63
AnthonySinitsa <itonyemail@gmail.com>
2023-11-09 04:05:17 +0000

    css

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import SignUpView
+from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
 from web_app.views import home
 
@@ -8,4 +9,7 @@ urlpatterns = [
   path('signup/', SignUpView.as_view(), name='signup'),
   path('login/', auth_views.LoginView.as_view(), name='login'),
   path('logout/', auth_views.LoginView.as_view(), name='logout'),
+  path('add-setup/', YourAddSetupView.as_view(), name='add_setup'),
+  path('favorites/', YourFavoritesView.as_view(), name='favorites'),
+  path('logout/', LogoutView.as_view(), name='logout'),
 ]
