486ea0d35fe016ac58a52ddacd8582c07c9d116a
AnthonySinitsa <itonyemail@gmail.com>
2023-11-09 03:13:16 +0000

    home url path

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import SignUpView
 from django.contrib.auth import views as auth_views
+from web_app.views import home
 
 urlpatterns = [
+  path('', home, name='home'),
   path('signup/', SignUpView.as_view(), name='signup'),
   path('login/', auth_views.LoginView.as_view(), name='login'),
   path('logout/', auth_views.LoginView.as_view(), name='logout'),
