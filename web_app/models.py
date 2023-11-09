3251ae3fd141740145accfcd492a6e7f2ed8d291
AnthonySinitsa <itonyemail@gmail.com>
2023-11-09 02:08:46 +0000

    setup model

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Setup(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def get_total_price(self):
+    # method to calculate totla price of setup
+    return sum(item.get_total_price() for item in self.items.all())
