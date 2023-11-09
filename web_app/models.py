72c547fc24d4ba6907dff6be0d207ffb24dedb3e
AnthonySinitsa <itonyemail@gmail.com>
2023-11-09 02:14:40 +0000

    item model

--- web_app/models.py
+++ web_app/models.py
@@ -12,3 +12,16 @@ class Setup(models.Model):
   def get_total_price(self):
     # method to calculate totla price of setup
     return sum(item.get_total_price() for item in self.items.all())
+  
+class Item(models.Model):
+  setup = models.ForeignKey(Setup, related_name='items', on_delete=models.CASCADE)
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  url = models.URLField()
+  image = models.ImageField(upload_to='item_images/')
+  quantity = models.PositiveIntegerField(default=1)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  
+  def get_total_price(self):
+    # method calculate total price for this item (price * quantity)
+    return self.price * self.quantity
